b9b6e505459efa2bd4ec35701e5e5880c2d68a28
Joao Fernandes <jsfernandes91@gmail.com>
2020-12-03 18:15:41 +0000

    Solve day3 part2

--- day3/executable_solution.rb
+++ day3/executable_solution.rb
@@ -1,5 +1,10 @@
 require_relative "solver"
 
 input_array = File.read("./input").split("\n").map { |row| row.split("") }
-part1_solution = Solver.new(input_array).solve(3, 1)
+solver = Solver.new(input_array)
+part1_solution = solver.solve(3, 1)
 puts "Part1 solution: #{part1_solution}"
+
+part2_solution_parts = [solver.solve(1, 1), part1_solution, solver.solve(5, 1), solver.solve(7, 1), solver.solve(1, 2)]
+part2_solution = part2_solution_parts.reduce(:*)
+puts "Part2 solution: #{part2_solution}"
